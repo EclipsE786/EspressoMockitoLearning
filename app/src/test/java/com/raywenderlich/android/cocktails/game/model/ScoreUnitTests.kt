50c518b365a3048a091b342b855ab6d9baf9a273
EclipsE786 <mr.nurmuxammad@gmail.com>
2021-06-16 21:48:58 +0500

    Initial commit

---
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/model/ScoreUnitTests.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.android.cocktails.game.model
+
+import org.junit.Test
+
+class ScoreUnitTests {
+
+    @Test
+    fun whenIncrementingScore_shouldIncrementCurrentScore(){
+        val score = Score()
+
+    }
+}
